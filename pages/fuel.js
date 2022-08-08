75c6d4b07281c75260c41af06ff4acc820a7abe8
Pasindu_Sahan <75581618+PasinduSahan001@users.noreply.github.com>
2022-08-08 17:51:13 +0530

    Error Fix
    Build optimization failed: found page without a React Component as default export in pages/component/data/fuelStation

--- pages/fuel.js
+++ pages/fuel.js
@@ -5,10 +5,58 @@ import Link from 'next/link'
 import Navbar from './component/Navbar'
 import { getUser } from "../src/graphql/queries";
 import { withSSRContext } from "aws-amplify";
-
-import { fuelStation } from "./component/data/fuelStation";
 import Footer from './component/Footer'
 
+const fuelStation_arr = [{
+  name: "C.F. De Mel & Sons (CEYPETCO Lanka Fuel Filling Station)",
+  cordinates: [79.8507024498247, 6.928805010711227],
+  P92: "✅",
+  P95: "✅",
+  D: "❌",
+  SD: "❌"
+
+},
+{
+  name: "Rajagiriya Filling Station",
+  cordinates: [79.89863542949045, 6.908857780368848],
+  P92: "✅",
+  P95: "✅",
+  D: "❌",
+  SD: "✅"
+},
+{
+  name: "IOC Lanka Fuel Filling Station",
+  cordinates: [79.86749463446658, 6.935112817167763,],
+  P92: "❌",
+  P95: "✅",
+  D: "❌",
+  SD: "✅"
+},
+
+{
+  name: "H K S RANASINGHE (Lanka IOC Fuel Filling Station",
+  cordinates: [79.85487010219421, 6.896753524151393],
+  P92: "✅",
+  P95: "✅",
+  D: "✅",
+  SD: "✅"
+},
+{
+  name: "Battaramulla Fuel Station",
+  cordinates: [79.91732418509156, 6.90308295962401],
+  P92: "❌",
+  P95: "✅",
+  D: "❌",
+  SD: "❌"
+},
+{
+  name: "CEYPETCO Lanka Fuel Filling Station ",
+  cordinates: [79.85744502280717, 6.889255017504663],
+  P92: "❌",
+  P95: "❌",
+  D: "❌",
+  SD: "❌"
+},]
 
 export async function getServerSideProps({ req, res }) {
   const { Auth, API } = withSSRContext({ req });
@@ -103,7 +151,7 @@ const Fuel = ({ user, mode }) => {
 
                           </tr>
 
-                          {fuelStation.map((x, i) => <tr key={i}>
+                          {fuelStation_arr.map((x, i) => <tr key={i}>
                             <td className='text-start text-wrap col-6'>{x.name}</td>
                             <td className='col-1'>{x.P92}</td>
                             <td className='col-1'>{x.P95}</td>
